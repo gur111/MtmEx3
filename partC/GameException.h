f6cd5e9d36162e7e8624d5793dfc909ca01cc930
Gur Telem <gur111telem@gmail.com>
2020-06-25 17:58:01 +0300

    Add GameException

---
+++ partC/GameException.h
@@ -0,0 +1,29 @@
+#ifndef MTMEX3_GAME_BASE_H
+#define MTMEX3_GAME_BASE_H
+#include <string>
+#include <typeinfo>
+
+#include "../Auxiliaries.h"
+
+namespace mtm {
+
+class GameException : public mtm::Exception {
+   protected:
+    std::string error = "A game related error has occurred: ";
+
+   public:
+    std::string what() noexcept { return error + typeid(*this).name(); }
+};
+
+class IllegalArgument : public GameException {};
+class IllegalCell : public GameException {};
+class CellEmpty : public GameException {};
+class MoveTooFar : public GameException {};
+class CellOccupied : public GameException {};
+class OutOfRange : public GameException {};
+class OutOfAmmo : public GameException {};
+class IllegalTarget : public GameException {};
+
+};  // namespace mtm
+
+#endif
