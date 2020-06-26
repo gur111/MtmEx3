7f530334c01a378f4aaea1bfae14e4094c2bce5e
Gur Telem <gur111telem@gmail.com>
2020-06-26 22:10:28 +0300

    Add stuff. Don't asl

--- partC/GameBoard.h
+++ partC/GameBoard.h
@@ -5,24 +5,29 @@
 #include <vector>
 
 #include "../Auxiliaries.h"
-#include "Character.h"
+#include "../partB/Matrix.h"
 #include "GameException.h"
 
 namespace mtm {
+
+template <class T>
 class GameBoard {
-    std::vector<std::vector<std::shared_ptr<Character>>> data;
+    std::vector<std::vector<std::shared_ptr<T>>> &data;
     int width_, height_;
 
    private:
-    std::shared_ptr<Character> &getCell(const GridPoint point);
+    static Dimensions verifyDimensions(Dimensions dims);
+     std::shared_ptr<T> &getCell(const GridPoint point);
 
    public:
     GameBoard(Dimensions dims);
+    ~GameBoard();
+    bool isWithinLimit(const GridPoint point) const;
     const int &width() const;
     const int &height() const;
-    void set(const GridPoint point, std::shared_ptr<Character> character);
-    std::shared_ptr<Character> get(const GridPoint point);
-    std::shared_ptr<Character> operator()(int i, int j);
+    void set(const GridPoint point, std::shared_ptr<T> character);
+    std::shared_ptr<T> get(const GridPoint point);
+    std::shared_ptr<T> operator()(int i, int j);
     // const std::shared_ptr<Character> get(GridPoint point) const;
 };
 };  // namespace mtm
