50bd28f57bab8a5f431d3ad9d9d7169a35128d8a
Gur Telem <gur111telem@gmail.com>
2020-06-26 22:14:31 +0300

    Implement as template. Also add functions: isWithinLimits static verifyDimensions

--- partC/GameBoard.cpp
+++ partC/GameBoard.cpp
@@ -3,36 +3,65 @@
 
 namespace mtm {
 
-GameBoard::GameBoard(Dimensions dims) {
+static Dimensions verifyDimensions(Dimensions dims) {
     if (dims.getRow() <= 0 || dims.getCol() <= 0) {
         throw IllegalArgument();
     }
+    return dims;
 }
 
-const int &GameBoard::height() const { return height_; }
-const int &GameBoard::width() const { return width_; }
+template <class T>
+GameBoard<T>::GameBoard(Dimensions dims)
+    : data(*(new std::vector<std::vector<std::shared_ptr<T>>>(
+          verifyDimensions(dims).getRow(),
+          std::vector<std::shared_ptr<T>>(verifyDimensions(dims).getCol(),
+                                          nullptr)))) {}
 
-std::shared_ptr<Character> &GameBoard::getCell(const GridPoint point) {
+template <class T>
+GameBoard<T>::~GameBoard() {
+    delete &data;
+}
+
+template <class T>
+const int &GameBoard<T>::height() const {
+    return height_;
+}
+
+template <class T>
+const int &GameBoard<T>::width() const {
+    return width_;
+}
+
+template <class T>
+std::shared_ptr<T> &GameBoard<T>::getCell(const GridPoint point) {
     if (point.row >= height() || point.col >= width()) {
         throw IllegalCell();
     }
     return data[point.row][point.col];
 }
 
-void GameBoard::set(const GridPoint point,
-                    std::shared_ptr<Character> character) {
+template <class T>
+void GameBoard<T>::set(const GridPoint point, std::shared_ptr<T> character) {
     getCell(point) = character;
 }
 
-std::shared_ptr<Character> GameBoard::get(const GridPoint point) {
+template <class T>
+std::shared_ptr<T> GameBoard<T>::get(const GridPoint point) {
     return getCell(point);
 }
 
-std::shared_ptr<Character> GameBoard::operator()(int i, int j) {
+template <class T>
+std::shared_ptr<T> GameBoard<T>::operator()(int i, int j) {
     return this->get(GridPoint(i, j));
 }
 
-// const std::shared_ptr<Character> GameBoard::get(GridPoint point) const{
+template <class T>
+bool GameBoard<T>::isWithinLimit(const GridPoint point) const {
+    return point.col > width() || point.row > height() || point.row < 0 ||
+           point.col < 0;
+}
+
+// const std::shared_ptr<T> GameBoard::get(GridPoint point) const{
 //     return getCell(point);
 // }
 
