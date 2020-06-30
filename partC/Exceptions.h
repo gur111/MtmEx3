f35ce5ff32fb81e9596ea5dcb287c6bfc381dc9c
Gur Telem <gur111telem@gmail.com>
2020-06-30 21:12:57 +0300

    Stupid way for giving exception error msg (what)

--- partC/Exceptions.h
+++ partC/Exceptions.h
@@ -2,6 +2,9 @@
 #define MTMEX3_GAME_BASE_H
 #include <string>
 #include <typeinfo>
+#include <iostream>
+
+#define DEFAULT_ERROR_PREFIX "A game related error has occurred: "
 
 #include "../Auxiliaries.h"
 
@@ -11,20 +14,62 @@ class Exception : public std::exception {};
 
 class GameException : public Exception {
    protected:
-    std::string error = "A game related error has occurred: ";
+    std::string error = "";
 
    public:
-    const char * what() const noexcept { return (error + typeid(*this).name()).c_str(); }
+    const char* what() const noexcept {
+        return (error + typeid(*this).name()).c_str();
+    }
 };
 
-class IllegalArgument : public GameException {};
-class IllegalCell : public GameException {};
-class CellEmpty : public GameException {};
-class MoveTooFar : public GameException {};
-class CellOccupied : public GameException {};
-class OutOfRange : public GameException {};
-class OutOfAmmo : public GameException {};
-class IllegalTarget : public GameException {};
+class IllegalArgument : public GameException {
+   public:
+    const char* what() const noexcept {
+        return DEFAULT_ERROR_PREFIX "IllegalArgument";
+    }
+};
+class IllegalCell : public GameException {
+       public:
+    const char* what() const noexcept {
+        return DEFAULT_ERROR_PREFIX "IllegalCell";
+    }
+};
+class CellEmpty : public GameException {
+       public:
+    const char* what() const noexcept {
+        return DEFAULT_ERROR_PREFIX "CellEmpty";
+    }
+};
+class MoveTooFar : public GameException {
+       public:
+    const char* what() const noexcept {
+        return DEFAULT_ERROR_PREFIX "MoveTooFar";
+    }
+};
+class CellOccupied : public GameException {
+       public:
+    const char* what() const noexcept {
+        return DEFAULT_ERROR_PREFIX "CellOccupied";
+    }
+};
+class OutOfRange : public GameException {
+       public:
+    const char* what() const noexcept {
+        return DEFAULT_ERROR_PREFIX "OutOfRange";
+    }
+};
+class OutOfAmmo : public GameException {
+       public:
+    const char* what() const noexcept {
+        return DEFAULT_ERROR_PREFIX "OutOfAmmo";
+    }
+};
+class IllegalTarget : public GameException {
+       public:
+    const char* what() const noexcept {
+        return DEFAULT_ERROR_PREFIX "IllegalTarget";
+    }
+};
 
 };  // namespace mtm
 
